baff9ba17480790527e7b5c414a587a3a085380b
Alinawaz <139918066+TypescriptAlinawaz@users.noreply.github.com>
2023-08-12 21:17:02 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favoriteNumber = 110;
+let message = `My Favorite number is : ${favoriteNumber}`;
+console.log(message);
